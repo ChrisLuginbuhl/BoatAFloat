c240225d6ab5f4fefdeaf0778bd51fca5588255d
Chris L <chris.luginbuhl@gmail.com>
2017-12-03 09:37:25 -0500

    Parsing Particle.subscribe() properly, Smoothed servo motion
    FIX: need to interleave programming servo motion with reading/publishing data better

--- BoatAFloat.ino
+++ BoatAFloat.ino
@@ -1,4 +1,3 @@
-//#include <ArduinoJson.h>
 #include <Adafruit_LSM9DS0.h>
 #include <math.h>
 
@@ -6,6 +5,7 @@
 
 Adafruit_LSM9DS0 lsm = Adafruit_LSM9DS0();
 
+const int servoPin = D2;
 const int numAvgs = 5000;
 const int NUM_CALIBRATION_READS = 5000;
 const int PUBLISH_INTERVAL = 2000; //milliseconds. Make sure this is long enough to capture at least one cycle of waves.
@@ -46,6 +46,8 @@ double maxRoll = 0;
 double minPitch = 0;
 double maxPitch = 0;
 
+Servo servoOne;   //create Servo object. LIbrary is included in particle firmware by default.
+
 void setupSensor()
 {
   // 1.) Set the accelerometer range
@@ -77,24 +79,29 @@ void setup()
   Serial.println("");
   delay(500); //let lsm readings stabilize;
 
-  //get the initial state of the lsm sensors
-  lsm.read();
-  //necessary to calibrate magnetometer data to be able to separate static from changing dynamic fields.
-  for (int i = 0; i < NUM_CALIBRATION_READS; i++) {
-    magSumX += (int)lsm.magData.x;
-    magSumY += (int)lsm.magData.y;
-    magSumZ += (int)lsm.magData.z;
-  }
+  servoOne.attach(D2);
+  servoOne.write(90);  // We try to leave the servo and start the servo in the same position to prevent a "lurch" - a full speed move to zero from wherever it was left.
 
-  initialMagX = magSumX / NUM_CALIBRATION_READS;
-  initialMagY = magSumY / NUM_CALIBRATION_READS;
-  initialMagZ = magSumZ / NUM_CALIBRATION_READS;
+  /*
+    //get the initial state of the lsm sensors
+    lsm.read();
+    //necessary to calibrate magnetometer data to be able to separate static from changing dynamic fields.
+    for (int i = 0; i < NUM_CALIBRATION_READS; i++) {
+      magSumX += (int)lsm.magData.x;
+      magSumY += (int)lsm.magData.y;
+      magSumZ += (int)lsm.magData.z;
+    }
 
-  //   initialAccelX = accelSumX / NUM_CALIBRATION_READS;
-  //   initialAccelY = accelSumY / NUM_CALIBRATION_READS;
-  //   initialAccelZ = accelSumZ / NUM_CALIBRATION_READS;
+    initialMagX = magSumX / NUM_CALIBRATION_READS;
+    initialMagY = magSumY / NUM_CALIBRATION_READS;
+    initialMagZ = magSumZ / NUM_CALIBRATION_READS;
 
-  //   Particle.variable("pitch", pitchAngle);   //use Particle.variable() if we want the website to request the data each time. Use .publish() if we want to broadcast
+    //   initialAccelX = accelSumX / NUM_CALIBRATION_READS;
+    //   initialAccelY = accelSumY / NUM_CALIBRATION_READS;
+    //   initialAccelZ = accelSumZ / NUM_CALIBRATION_READS;
+
+    //   Particle.variable("pitch", pitchAngle);   //use Particle.variable() if we want the website to request the data each time. Use .publish() if we want to broadcast
+  */
 
   Particle.subscribe("totalRoll", moveBoat);
 }
@@ -136,7 +143,7 @@ void loop()
   totalRoll = sqrt(pow((maxRoll - minRoll), 2) + pow((maxPitch - minPitch), 2)); //pythagoras's theorem. Used for finding the max angle from vertical using pitch and roll angles.
   Particle.publish("totalRoll ", String(int(totalRoll)), 1);
 
-  delay(400);
+  delay(500); //from the Particle docs: "The built-in delay function safely interleaves required background activity, so arbitrarily long delays can safely be done if you need them."
 
   Serial.print("Total roll: ");
   Serial.print(totalRoll);
@@ -197,10 +204,45 @@ void getRollAndPitch() {
 
 void moveBoat(const char *tRoll, const char *data)
 {
-  Serial.print("Total Roll: ");
-  if (data)
+  int angle = 90;
+  int amplitude = 0;
+  int roll = 0;
+  String data1 = "";
+  std::string s = data;
+
+  Serial.print("Total Roll received: ");
+  if (data) {
     Serial.println(data);
+  }
   else
     Serial.println("NULL");
+
+  int commaIndex = s.length();
+  for (int i = 0; i < commaIndex; i++) {
+    data1 = data1 + data[i];
+  }
+
+  roll = data1.toInt();
+
+
+  if (roll < 2) {
+    amplitude = 25;
+    Serial.println("Sinwave 4");
+  } else if (roll < 10) {
+    amplitude = 50;
+    Serial.println("Sinewave 8");
+  } else if (roll < 30) {
+    amplitude = 75;
+  } else if (roll >= 30) {
+    amplitude = 120;
+  }
+
+  for (int i = 0; i < 360 * 10; i++) {
+    servoOne.writeMicroseconds(1500 + (int)(amplitude * sin(i * PI / 180))); //1500 microseconds corresponds to 90degress. The full motion range for 9g servos is usually 1000-2000us.
+    delay(PUBLISH_INTERVAL / 180);
+  }
+
+  servoOne.write(90);
 }
+
 
